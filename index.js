34f87508502fd2f91ded4b216a9800dcdf9a406b
JewelHFahim <jewelhfahim@gmail.com>
2022-12-26 23:16:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.OS_USER}:${process.env.OS_PASS}@cluster0.qez1k8e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    const productsCollection = client.db("healthOS").collection("products");
+    const categoriesCollection = client.db("healthOS").collection("categories");
+    const usersCollection = client.db("healthOS").collection("users");
+
+    try{
+
+        // Products
+        app.post('/products', async(req, res)=>{
+            const products = req.body;
+            const result = await productsCollection.insertOne(products);
+            res.send(products)
+        })
+        app.get('/products', async(req, res)=>{
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+        app.get('/products/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
+        app.delete('/products/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+        app.patch('/products/:id',  async(req, res)=>{
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = {_id: ObjectId(id)};
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await productsCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+
+        // Category
+        app.post('/categories', async(req, res)=>{
+            const category = req.body;
+            const result = await categoriesCollection.insertOne(category);
+            res.send(category)
+        })
+        app.get('/categories', async(req, res)=>{
+            const query = {};
+            const category = await categoriesCollection.find(query).toArray();
+            res.send(category);
+        })
+        app.get('/categories/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await categoriesCollection.findOne(query);
+            res.send(result);
+        })
+        app.delete('/categories/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await categoriesCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // Users
+        app.get('/users', async(req, res)=>{
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+        app.get('/users/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+        })
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally{
+    }
+}
+run().catch(error=>console.log(error))
+
+
+app.get('/', (req, res)=>{
+    res.send('HealthOS server running...!!')
+})
+
+app.listen(port, (req, res)=>{
+    console.log('HealthOS-', port);
+})
+
+module.exports = app;
